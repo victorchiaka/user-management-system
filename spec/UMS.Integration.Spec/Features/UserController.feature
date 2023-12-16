d85a92c4609057827efac8c65b14671e4d30a206
victorchiaka <chiakavictor205@gmail.com>
2023-12-16 22:14:29 +0100

    ft: implemented integration test for some endpoints

--- spec/UMS.Integration.Spec/Features/UserController.feature
+++ spec/UMS.Integration.Spec/Features/UserController.feature
@@ -4,5 +4,48 @@ In order accurately manage everything related to creating users account and over
 I want to register user, delete user and perform any other operation related to managing the users account
 
 Scenario: Register a new user with valid data
-    Given a user is registered with valid data
-    Then the response status code should be 201 Created
+    Given A user is registered with valid data
+    Then The response status code should be 201 Created
+    
+Scenario: Attempting to register user with an already existing email
+    Given A user tries to register with an already existing email
+    Then The response status code should be 400 Bad Request
+    
+Scenario: A user login in with valid data
+    When A user login with valid data
+    Then The response status code should be 200 Ok
+    And Jwt parameter should not be null or empty
+    
+Scenario: A user tries to login using an invalid data
+    When A user tries to login using invalid data
+    Then The response status code should be 400 Bad Request
+    
+Scenario: Update username field if a user is authenticated
+    Given A user is authenticated with an ID of "3"
+    When The user updates its username
+    Then The response status code should be 200 Ok
+    
+    
+Scenario: A user that doesn't exist trying to access the UpdateUsername endpoint
+    When The user attempts to update username
+    Then The response status code should be 404 NotFound
+    
+    
+Scenario: Update Email field if a user is authenticated
+    Given A user is authenticated with an ID of "4"
+    When The user updates its email address
+    Then The response status code should be 200 Ok
+
+
+Scenario: A user that doesn't exist trying to access the UpdateEmailAddress endpoint
+    When The user attempts to update email address
+    Then The response status code should be 404 NotFound
+    
+Scenario: Update password field if a user authenticated
+    Given A user is authenticated with an ID of "2"
+    When The user resets its password with valid data
+    Then The response status code should be 200 Ok
+    
+Scenario: A user that doesn't exist trying to access the UpdatePassword endpoint
+    When The user attempts to update password
+    Then The response status code should be 404 NotFound
--- spec/UMS.Integration.Spec/Steps/UserControllerStepDefinitions.cs
+++ spec/UMS.Integration.Spec/Steps/UserControllerStepDefinitions.cs
@@ -1,6 +1,10 @@
 ﻿using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.IdentityModel.Tokens;
+using UMS.Api.DTOs;
 using UMS.Integration.Spec.Hooks;
 using Xunit;
 
@@ -15,17 +19,27 @@ public sealed class UserControllerStepDefinition
 
     public UserControllerStepDefinition(CustomWebApplicationFactory<Program> customWebApplicationFactory)
     {
+        
         this._customWebApplicationFactory = customWebApplicationFactory;
         this._httpClient = _customWebApplicationFactory.CreateClient(new WebApplicationFactoryClientOptions
         {
             AllowAutoRedirect = false
         });
+        
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzAyODM0MjEwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUxNjYiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUxNjYifQ.-ABg4nt_y-st3aIT-yxJDA5OpZNQ_nLdaxF6tXcFIXpjqL029wEZ1_CU7aDgH93Fr_6iFzqPromhH65iikHKeQ");
     }
 
-    [Given(@"a user is registered with valid data")]
+    [Given(@"A user is authenticated with an ID of ""(.*)""")]
+    public void GivenAUserIsAuthenticatedWithAnIdOf(string userId)
+    {
+        _httpClient.DefaultRequestHeaders.Add(TestAuthenticationHandler.UserId, userId);
+    }
+
+    [Given(@"A user is registered with valid data")]
     public void GivenAUserIsRegisteredWithValidData()
     {
-        var registerUserRequestDto = new
+
+        RegisterUserRequestDto registerUserRequestDto = new()
         {
             Username = "johnDoe",
             EmailAddress = "johndoe@mail.com",
@@ -36,9 +50,157 @@ public sealed class UserControllerStepDefinition
             registerUserRequestDto).Result;
     }
 
-    [Then(@"the response status code should be (.*) Created")]
+    [Then(@"The response status code should be (.*) Created")]
     public void ThenTheResponseStatusCodeShouldBeCreated(int expectedStatusCode)
     {
-        Assert.Equal((HttpStatusCode)expectedStatusCode, _httpResponseMessage.StatusCode);
+        _httpResponseMessage.StatusCode.Should().Be((HttpStatusCode)expectedStatusCode);
+    }
+    
+    [Given(@"A user tries to register with an already existing email")]
+    public void GivenAUserTriesToRegisterWithAnAlreadyExistingEmail()
+    {
+        RegisterUserRequestDto registerUserRequestDto = new()
+        {
+            Username = "johnDoe",
+            EmailAddress = "johndoe@mail.com",
+            Password = "password"
+        };
+
+        _httpResponseMessage = _httpClient.PostAsJsonAsync("api/v1/User/Register",
+            registerUserRequestDto).Result;
+    }
+    
+    [When(@"A user login with valid data")]
+    public void WhenAUserLoginWithValidData()
+    {
+        LoginUserRequestDto loginUserRequestDto = new()
+        {
+            EmailAddress = "johndoe@mail.com",
+            Password = "password"
+        };
+
+        _httpResponseMessage = _httpClient.PostAsJsonAsync("api/v1/User/Login", loginUserRequestDto).Result;
+    }
+
+    [When(@"A user tries to login using invalid data")]
+    public void WhenAUserTriesToLoginUsingInvalidData()
+    {
+        LoginUserRequestDto loginUserRequestDto = new()
+        {
+            EmailAddress = "johndoe@mail.com",
+            Password = "wrongPassword"
+        };
+
+        _httpResponseMessage = _httpClient.PostAsJsonAsync("api/v1/User/Login", loginUserRequestDto).Result;
+    }
+    
+    [Then(@"The response status code should be (.*) Bad Request")]
+    public void ThenTheResponseStatusCodeShouldBeBadRequest(int expectedStatusCode)
+    {
+        _httpResponseMessage.StatusCode.Should().Be((HttpStatusCode)expectedStatusCode);
+    }
+
+    [When(@"The user resets its password with valid data")]
+    public void WhenTheUserResetsItsPasswordWithValidData()
+    {
+        UpdatePasswordRequestDto updatePasswordRequestDto = new()
+        {
+            EmailAddress = "johndoe@mail.com",
+            NewPassword = "password"
+        };
+            
+        _httpResponseMessage = _httpClient.PutAsJsonAsync("api/v1/User/UpdatePassword",
+            updatePasswordRequestDto).Result;
+    }
+
+    [Then(@"The response status code should be (.*) Ok")]
+    public void ThenTheResponseStatusCodeShouldBeOk(int expectedStatusCode)
+    {
+        _httpResponseMessage.StatusCode.Should().Be((HttpStatusCode)expectedStatusCode);
+    }
+    
+    [When(@"The user attempts to update password")]
+    public void WhenTheUserAttemptsToUpdatePassword()
+    {
+        UpdatePasswordRequestDto updatePasswordRequestDto = new()
+        {
+            EmailAddress = "janedoe@mail.com",
+            NewPassword = "password"
+        };
+
+        _httpResponseMessage =
+            _httpClient.PutAsJsonAsync("api/v1/User/UpdatePassword", updatePasswordRequestDto).Result;
+    }
+
+    [Then(@"The response status code should be (.*) NotFound")]
+    public void ThenTheResponseStatusCodeShouldBeNotFound(int expectedStatusCode)
+    {
+        _httpResponseMessage.StatusCode.Should().Be((HttpStatusCode)expectedStatusCode);
+    }
+
+    [Then(@"Jwt parameter should not be null or empty")]
+    public void ThenJwtParameterShouldNotBeNullOrEmpty()
+    {
+        if (_httpResponseMessage.Headers.TryGetValues("Authorization", out IEnumerable<string>? tokenValues))
+        {
+            string? jwtToken = tokenValues.FirstOrDefault();
+            jwtToken.Should().NotBeNullOrEmpty();
+        }
+    }
+
+    [When(@"The user updates its username")]
+    public void WhenTheUserUpdatesItsPassword()
+    {
+        UpdateUsernameRequestDto updateUsernameRequestDto = new()
+        {
+            EmailAddress = "johndoe@mail.com",
+            Password = "password",
+            NewUsername = "johnWik"
+        };
+
+        _httpResponseMessage =
+            _httpClient.PutAsJsonAsync("api/v1/User/UpdateUsername", updateUsernameRequestDto).Result;
+    }
+
+    [When(@"The user attempts to update username")]
+    public void WhenTheUserAttemptsToUpdateUsername()
+    {
+        UpdateUsernameRequestDto updateUsernameRequestDto = new()
+        {
+            EmailAddress = "joe@mail.com",
+            Password = "password",
+            NewUsername = "bigJow"
+        };
+
+        _httpResponseMessage =
+            _httpClient.PutAsJsonAsync("api/v1/User/UpdateUsername", updateUsernameRequestDto).Result;
+    }
+    
+    [When(@"The user updates its email address")]
+    public void WhenTheUserUpdatesItsEmailAddress()
+    {
+        UpdateEmailAddressRequestDto updateEmailAddressRequestDto = new()
+        {
+            EmailAddress = "johndoe@mail.com",
+            Password = "password",
+            NewEmailAddress = "johndoe@mail.com"
+        };
+
+        _httpResponseMessage =
+            _httpClient.PutAsJsonAsync("api/v1/User/UpdateEmailAddress", updateEmailAddressRequestDto).Result;
+    }
+
+    [When(@"The user attempts to update email address")]
+    public void WhenTheUserAttemptsToUpdateEmailAddress()
+    {
+        UpdateEmailAddressRequestDto updateEmailAddressRequestDto = new()
+        {
+            EmailAddress = "joe@mail.com",
+            Password = "password",
+            NewEmailAddress = "bigjoe@mail.com"
+        };
+
+        _httpResponseMessage =
+            _httpClient.PutAsJsonAsync("api/v1/User/UpdateEmailAddress", updateEmailAddressRequestDto).Result;
     }
 }
